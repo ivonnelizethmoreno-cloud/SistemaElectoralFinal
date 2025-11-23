1a8a44c0e4421a6d684b9ecc2c3569ead5edf877
ivonnelizethmoreno-cloud <ivonnelizethmoreno@gmail.com>
2025-11-23 17:30:24 -0500

    Create RunCucumberTest.java

---
+++ src/test/java/com/elecciones/sistema/RunCucumberTest.java
@@ -0,0 +1,19 @@
+package com.elecciones.sistema;
+
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.ConfigurationParameter;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.SNIPPET_TYPE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features") 
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.elecciones.sistema.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json")
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
+public class RunCucumberTest { }
