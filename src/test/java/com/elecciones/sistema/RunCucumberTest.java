7bfa1a9e7d37b7687ab7aa4353a366978cc924d8
jhonjairosolerumbarila-ops <jhonjairosolerumbarila@gmail.com>
2025-11-27 19:55:52 -0500

    Update RunCucumberTest.java

--- src/test/java/com/elecciones/sistema/RunCucumberTest.java
+++ src/test/java/com/elecciones/sistema/RunCucumberTest.java
@@ -1,27 +1,7 @@
 package com.elecciones.sistema;
 
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.ConfigurationParameter;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.SNIPPET_TYPE_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")  // 👉 tus .feature están en src/test/resources/features
-@ConfigurationParameter(
-        key = GLUE_PROPERTY_NAME,
-        value = "com.elecciones.sistema.steps"  // 👉 ruta EXACTA donde está AppSteps.java
-)
-@ConfigurationParameter(
-        key = PLUGIN_PROPERTY_NAME,
-        value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json"
-)
-@ConfigurationParameter(
-        key = SNIPPET_TYPE_PROPERTY_NAME,
-        value = "camelcase"
-)
-public class RunCucumberTest { }
+@Cucumber
+public class RunCucumberTest {
+}
