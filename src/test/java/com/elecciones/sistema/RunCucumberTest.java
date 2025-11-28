870226211006511ba4040f642145dc72d490a53d
jhonjairosolerumbarila-ops <jhonjairosolerumbarila@gmail.com>
2025-11-27 22:41:26 -0500

    Update RunCucumberTest.java

--- src/test/java/com/elecciones/sistema/RunCucumberTest.java
+++ src/test/java/com/elecciones/sistema/RunCucumberTest.java
@@ -3,8 +3,4 @@ package com.elecciones.sistema;
 import io.cucumber.junit.platform.engine.Cucumber;
 
 @Cucumber
-@io.cucumber.junit.platform.engine.ConfigurationParameter(
-    key = io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME,
-    value = "classpath:features"
-)
 public class RunCucumberTest { }
